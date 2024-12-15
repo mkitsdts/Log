dee12027b596480be4a34a6c1f4c9cf23d040ced
mkitsdts <mkitsdts@outlook.com>
2024-12-15 17:19:04 +0800

    fixed some bugs and added some test example

--- log.h
+++ log.h
@@ -1,42 +1,142 @@
-#ifndef LOG_H
-#define LOG_H
-
+#pragma once
+#include <iostream>
+#include <fstream>
 #include <queue>
 #include <string>
 #include <mutex>
 #include <thread>
+#include <condition_variable>
 
-#define MAX_LOG_SIZE 1024;
-#define INFO 0
-#define DEBUG 1
-#define WARN 2
-#define ERROR 3
+const std::string SAVING_PATH = "./log.txt";        // 日志保存路径
+constexpr auto MAX_LOG_SIZE = 1024;                 // 日志最大缓冲数量
+constexpr auto DEBUG = 0;
+constexpr auto INFO = 1;
+constexpr auto WARN = 2;
+constexpr auto ERROR = 3;                           
 
 class Log
 {
 public:
-    static Log* instance();
-    void write(std::string& message, int level = 0);
-    void write(std::string&& message, int level = 0);
-    
+    inline static Log* instance(){
+        if (log == nullptr)
+        {
+            std::lock_guard<std::mutex> lock(create_mux);
+            if (log == nullptr) // Double-checked locking
+                log = new Log();
+        }
+        return log;
+    }
+
+    void add(const std::string& message, int level = INFO){
+        switch(level){
+        case INFO:{
+            std::lock_guard<std::mutex> lock(add_mux);
+            std::string tmp = "[INFO]:" + message;
+            log_queue.push(tmp);
+            if(log_queue.size() > MAX_LOG_SIZE)
+                flush();
+            break;
+        }
+        case DEBUG:{
+            std::lock_guard<std::mutex> lock(add_mux);
+            std::string tmp = "[DEBUG]:" + message;
+            log_queue.push(tmp);
+            if(log_queue.size() > MAX_LOG_SIZE)
+                flush();
+            break;
+        }
+        case WARN:{
+            std::lock_guard<std::mutex> lock(add_mux);
+            std::string tmp = "[WARN]:" + message;
+            log_queue.push(tmp);
+            flush();
+            break;
+        }
+        case ERROR:{
+            std::lock_guard<std::mutex> lock(add_mux);
+            std::string tmp = "[ERROR]:" + message;
+            log_queue.push(tmp);
+            std::cout<<tmp<<std::endl;
+            flush();
+            break;
+        }
+        default:
+            break;
+        }
+    }
+
+    void add(std::string&& message, int level = INFO){
+        switch(level){
+        case INFO:{
+            std::lock_guard<std::mutex> lock(add_mux);
+            std::string tmp = "[INFO]:" + message;
+            log_queue.push(tmp);
+            if(log_queue.size() > MAX_LOG_SIZE)
+                flush();
+            break;
+        }
+        case DEBUG:{
+            std::lock_guard<std::mutex> lock(add_mux);
+            std::string tmp = "[DEBUG]:" + message;
+            log_queue.push(tmp);
+            if(log_queue.size() > MAX_LOG_SIZE)
+                flush();
+            break;
+        }
+        case WARN:{
+            std::lock_guard<std::mutex> lock(add_mux);
+            std::string tmp = "[WARN]:" + message;
+            log_queue.push(tmp);
+            flush();
+            break;
+        }
+        case ERROR:{
+            std::lock_guard<std::mutex> lock(add_mux);
+            std::string tmp = "[ERROR]:" + message;
+            log_queue.push(tmp);
+            std::cout<<tmp<<std::endl;
+            flush();
+            break;
+        }
+        default:
+            break;
+        }
+    }
+
 private:
-    Log();
-    ~Log();
-    void flush();
+    Log() = default;
+    ~Log(){
+        flush();
+    }
+
+    void flush(){
+        auto f = [this](){
+        std::lock_guard<std::mutex> lock(flush_mux);
+        if(this->log_queue.empty()){
+            return;
+        }
+        this->log_queue_tmp = std::move(this->log_queue);
+        this->log_queue = std::queue<std::string>{};
+        std::ofstream out(SAVING_PATH, std::ios::app);
+        while(!(this->log_queue_tmp).empty()){
+            out << (this->log_queue_tmp).front() << "\n";
+            (this->log_queue_tmp).pop();
+        }
+        out.close();
+        };
+        std::thread t(f);
+        t.detach();
+    }
 private:
     std::queue<std::string> log_queue;
     std::queue<std::string> log_queue_tmp;
-    std::mutex mux;
-    static Log* log;
-    std::string path;
+    std::mutex flush_mux;
+    std::mutex add_mux;
+    inline static std::mutex create_mux{};
+    inline static Log* log = nullptr;
 };
 
-#define LOG_INFO(message) Log::instance()->write(message, 1)
-
-#define LOG_DEBUG(message) Log::instance()->write(message, 0)
-
-#define LOG_WARN(message) Log::instance()->write(message, 2)
-
-#define LOG_ERROR(message) Log::instance()->write(message, 3)
-
-#endif // LOG_H
+#define LOG_INFO(message) Log::instance()->add(message, 1)
+#define LOG_DEBUG(message) Log::instance()->add(message, 0)
+#define LOG_WARN(message) Log::instance()->add(message, 2)
+#define LOG_ERROR(message) Log::instance()->add(message, 3)
---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include "log.h"
+#include <chrono>
+int main()
+{
+    auto log = Log::instance();
+    log->add("Hello, world!", INFO);
+    log->add("Hello, world!", DEBUG);
+    log->add("Hello, world!", WARN);
+    log->add("Hello, world!", ERROR);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    return 0;
+}
