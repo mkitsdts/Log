a6296f9d0f6099b6efc322c697412faa4cd271bd
mkitsdts <mkitsdts@outlook.com>
2024-12-25 12:17:03 +0800

    update test example

--- test.cpp
+++ test.cpp
@@ -1,14 +1,32 @@
 #include "log.h"
-#include <chrono>
 
 int main()
 {
-    auto log = Log::instance();
-    log->add("Hello, world!", INFO);
-    log->add("Hello, world!", DEBUG);
-    log->add("Hello, world!", WARN);
-    log->add("Hello, world!", ERROR);
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::thread t1([](){
+        for(int i = 0; i < 100; i++){
+            Log::instance()->add("This is a debug message", DEBUG);
+        }
+    });
+    std::thread t2([](){
+        for(int i = 0; i < 100; i++){
+            Log::instance()->add("This is a info message", INFO);
+        }
+    });
+    std::thread t3([](){
+        for(int i = 0; i < 100; i++){
+            Log::instance()->add("This is a warning message", WARN);
+        }
+    });
+    std::thread t4([](){
+        for(int i = 0; i < 100; i++){
+            Log::instance()->add("This is a error message", ERROR);
+        }
+    });
+    t1.detach();
+    t2.detach();
+    t3.detach();
+    t4.detach();
+    std::this_thread::sleep_for(std::chrono::seconds(10));
     getchar();
     getchar();
     return 0;
